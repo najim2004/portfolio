de795fd1993b8a67db5b21ccac5f5b826e3a1b44
najim2004 <najim10112004@gmail.com>
2024-06-19 20:03:41 +0600

    added banner section

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,18 +2,34 @@ import React from "react";
 import Image from "../assets/avatar.svg";
 import { FaGithub, FaLinkedin, FaFacebook } from "react-icons/fa";
 import { TypeAnimation } from "react-type-animation";
-
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
 const Banner = () => {
   return (
-    <section id="home" className="section">
+    <section
+      id="home"
+      className="min-h-[85vh] lg:min-h-[78vh] flex items-center"
+    >
       <div className="container mx-auto">
-        <div className="">
+        <div className="flex flex-col lg:flex-row gap-y-8 lg:items-center lg:gap-x-12">
           {/* text */}
-          <div className="">
-            <h1>
-              BEN <span>AIDEN</span>
-            </h1>
-            <div className="mb-6 text-[36px] lg:text-[60px] font-secondary uppercase leading-1">
+          <div className="flex-1 text-center font-secondary lg:text-left">
+            <motion.h1
+              variants={fadeIn("up", 0.3)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="text-[55px] font-bold leading-[0.8] lg:text-[110px]"
+            >
+              NA<span>J</span>IM
+            </motion.h1>
+            <motion.div
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-6 text-[36px] lg:text-[60px] font-secondary uppercase leading-1"
+            >
               <span className="mr-4 text-white">I am a</span>
               <TypeAnimation
                 sequence={[
@@ -29,20 +45,58 @@ const Banner = () => {
                 wrapper="span"
                 repeat={Infinity}
               />
-            </div>
-            <p>
+            </motion.div>
+            <motion.p
+              variants={fadeIn("up", 0.5)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-8 max-w-max mx-auto lg:mx-0"
+            >
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti,
               qui. Maxime magnam asperiores amet sed consequatur distinctio
               voluptatibus totam nam.
-            </p>
-            <div className="">
+            </motion.p>
+            <motion.div
+              variants={fadeIn("up", 0.6)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0"
+            >
               <button className="btn btn-lg">Contact me</button>
-            </div>
+              <a href="#" className="btn-link text-gradient">
+                My Resume
+              </a>
+            </motion.div>
+            {/* social links */}
+            <motion.div
+              variants={fadeIn("up", 0.7)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0"
+            >
+              <a href="">
+                <FaFacebook />
+              </a>
+              <a href="">
+                <FaLinkedin />
+              </a>
+              <a href="">
+                <FaGithub />
+              </a>
+            </motion.div>
           </div>
           {/* image */}
-          <div className="">
+          <motion.div
+            variants={fadeIn("down", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            className="hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px]"
+          >
             <img src={Image} alt="avatar" />
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
