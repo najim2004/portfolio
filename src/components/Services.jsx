8f86ec7690b5d3d37f58548393ecca3015b50703
najim2004 <najim10112004@gmail.com>
2024-06-22 00:52:36 +0600

    work section done

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,9 +1,87 @@
 import React from "react";
-
+import { motion } from "framer-motion";
+import { BsArrowUpRight } from "react-icons/bs";
+import { fadeIn } from "../variants";
+const services = [
+  {
+    name: "UI/UX Design",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non ad iusto sapiente inventore aliquam delectus iure dolorum et. Sunt distinctio eos, quo illo eaque commodi voluptates dolor eligendi iusto doloremque.",
+    link: "Learn more",
+  },
+  {
+    name: "Development",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non ad iusto sapiente inventore aliquam delectus iure dolorum et. Sunt distinctio eos, quo illo eaque commodi voluptates dolor eligendi iusto doloremque.",
+    link: "Learn more",
+  },
+  {
+    name: "Digital Marketing",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non ad iusto sapiente inventore aliquam delectus iure dolorum et. Sunt distinctio eos, quo illo eaque commodi voluptates dolor eligendi iusto doloremque.",
+    link: "Learn more",
+  },
+];
 const Services = () => {
   return (
     <div id="services" className="section">
-      Services
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row">
+          {/* text */}
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 lg:bg-services bg-contain lg:bg-bottom bg-no-repeat mix-blend-lighten mb-12 lg:mb-0"
+          >
+            <h2 className="h2 text-accent mb-6">What I Do</h2>
+            <h3 className="h3 max-w-[455px] mb-16">
+              I'm a Freelance Front-end Developer with over 5 years of
+              experience.
+            </h3>
+            <button className="btn btn-sm ">See my work</button>
+          </motion.div>
+          {/* Services */}
+          <motion.div
+            variants={fadeIn("left", 0.4)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1"
+          >
+            {/* service list */}
+            <div className="">
+              {services?.map((service, i) => (
+                <div
+                  key={i}
+                  className="border-b border-white/20 h-[146px] mb-[38px] flex"
+                >
+                  <div className="max-w-[476px] ">
+                    <h4 className="text-xl tracking-wider font-primary font-semibold mb-6">
+                      {service.name}
+                    </h4>
+                    <p className="font-secondary leading-tight">
+                      {service.description}
+                    </p>
+                  </div>
+                  <div className="">
+                    <a
+                      href="#"
+                      className="btn w-9 h-9 mb-[42px] flex justify-center items-center"
+                    >
+                      <BsArrowUpRight />
+                    </a>
+                    <a href="#" className="text-gradient text-sm">
+                      {service.link}
+                    </a>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </motion.div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,9 +1,104 @@
 import React from "react";
+import { motion } from "framer-motion";
+import Img1 from "../assets/portfolio-img1.png";
+import Img2 from "../assets/portfolio-img2.png";
+import Img3 from "../assets/portfolio-img3.png";
+import { fadeIn } from "../variants";
 
 const Work = () => {
   return (
     <div id="work" className="section">
-      Work
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row gap-x-10">
+          {/* text */}
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0"
+          >
+            <div>
+              <h2 className="h2 leading-tight text-accent">
+                My Latest <br />
+                work
+              </h2>
+              <p className="max-w-sm mb-16">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo
+                quae optio, dolore ea veritatis quam, sunt consequatur harum
+              </p>
+              <button className="btn btn-sm">View all projects</button>
+            </div>
+            {/* img */}
+            <div className=" group relative overflow-hidden border-2 border-white/20 rounded-xl">
+              {/* overlay */}
+              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+              {/* img */}
+              <img
+                className="group-hover:scale-125 transition-all duration-500"
+                src={Img1}
+                alt=""
+              />
+              {/* pretitle */}
+              <div className="absolute -bottom-full left-1/2 group-hover:bottom-24 transition-all duration-500 z-50 -translate-x-[50%]">
+                <span className="text-gradient">UI/UX Design</span>
+              </div>
+              {/* title */}
+              <div className="absolute -bottom-full left-1/2 group-hover:bottom-16 transition-all duration-700 z-50 -translate-x-[50%]">
+                <span className="text-white text-3xl">Project Title</span>
+              </div>
+            </div>
+          </motion.div>
+          <motion.div
+            variants={fadeIn("left", 0.2)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 flex flex-col gap-y-10"
+          >
+            {/* 2 */}
+            {/* img */}
+            <div className=" group relative overflow-hidden border-2 border-white/20 rounded-xl">
+              {/* overlay */}
+              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+              {/* img */}
+              <img
+                className="group-hover:scale-125 transition-all duration-500"
+                src={Img2}
+                alt=""
+              />
+              {/* pretitle */}
+              <div className="absolute -bottom-full left-1/2 group-hover:bottom-24 transition-all duration-500 z-50 -translate-x-[50%]">
+                <span className="text-gradient">UI/UX Design</span>
+              </div>
+              {/* title */}
+              <div className="absolute -bottom-full left-1/2 group-hover:bottom-16 transition-all duration-700 z-50 -translate-x-[50%]">
+                <span className="text-white text-3xl">Project Title</span>
+              </div>
+            </div>
+            {/* 3 */}
+            {/* img */}
+            <div className=" group relative overflow-hidden border-2 border-white/20 rounded-xl">
+              {/* overlay */}
+              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+              {/* img */}
+              <img
+                className="group-hover:scale-125 transition-all duration-500"
+                src={Img3}
+                alt=""
+              />
+              {/* pretitle */}
+              <div className="absolute -bottom-full left-1/2 group-hover:bottom-24 transition-all duration-500 z-50 -translate-x-[50%]">
+                <span className="text-gradient">UI/UX Design</span>
+              </div>
+              {/* title */}
+              <div className="absolute -bottom-full left-1/2 group-hover:bottom-16 transition-all duration-700 z-50 -translate-x-[50%]">
+                <span className="text-white text-3xl">Project Title</span>
+              </div>
+            </div>
+          </motion.div>
+        </div>
+      </div>
     </div>
   );
 };
